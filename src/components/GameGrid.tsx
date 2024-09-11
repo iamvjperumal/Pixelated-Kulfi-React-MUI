6703cf463693e3645fe3b6956df330f15a1b5ab0
Vijayperumal <>
2024-09-11 09:46:25 +0530

    Updated Games to hooks

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,28 +1,11 @@
-import {useEffect, useState} from 'react'
-import apiClients from '../services/apiClients';
 import { Typography } from '@mui/material';
+import useGames from '../hooks/useGames';
 
-interface Game {
-    id: number,
-    name: string
-}
 
-interface FetchGameResponse {
-    count: number,
-    results: Game[]
-}
 
 const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
 
-    useEffect(() => {
-      apiClients.get<FetchGameResponse>('/games')
-      .then(res => setGames(res.data.results))
-      .catch(err => setError(err.message));
-    })
-    
-    
+    const {games, error} = useGames();
   return (
     <>
      <div>GameGrid</div>
