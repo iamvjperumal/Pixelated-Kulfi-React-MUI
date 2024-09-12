534f94568b299c1f053392f3a59309391e8a8ba4
Vijayperumal <>
2024-09-12 09:53:04 +0530

    Updated Build ready code

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import PixelatedLogo from "./assets/PIXELATED.png";
+//import PixelatedLogo from "./assets/PIXELATED.png";
 // import MenuIcon from '@mui/icons-material/Menu';
 import Grid from "@mui/material/Grid2";
 import ListSubheader from "@mui/material/ListSubheader";
