825a1136c0a929859db16685f9b4a9fd3a1386f4
Vijayperumal <>
2024-09-12 09:55:46 +0530

    Updated Gaming Seketon upto 12

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ interface GameGridProps {
 }
 const GameGrid = ({ selectedGenre }: GameGridProps) => {
   const { data, isLoading, error } = useGames(selectedGenre);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <>
       {error && <Typography variant="h5">{error}</Typography>}
